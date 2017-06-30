f04549f369002f2491eef5eb775bfcefc9bfc1f9
coderantasoft <coder@antasoft.com>
2017-06-30 18:44:45 +0530

    Create myfile.php

---
+++ myfile.php
@@ -0,0 +1,8 @@
+<?php
+http://localhost/kundan/rajiv_word/store/
+http://localhost/kundan/rajiv_word/library/videos/
+
+http://localhost/kundan/rajiv_word/library/knowledge-bytes/
+http://localhost/kundan/rajiv_word/infinity-foundation/swadeshi-indology/
+
+?>
